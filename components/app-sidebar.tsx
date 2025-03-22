af81a57def77afff5a361167ba4b996776d94d1a
Paololibert <libertsinzo7@gmail.com>
2025-03-22 13:08:48 +0100

    update logo

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -84,8 +84,8 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <a href="#">
-                <span className="text-base font-semibold">PCo</span>
+              <a href="/">
+                <span className="text-base font-semibold">PCollaborative</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
