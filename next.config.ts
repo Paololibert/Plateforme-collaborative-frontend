eb5049366cce40f2523cc457528ed878b4c12a50
Paololibert <libertsinzo7@gmail.com>
2025-03-22 14:49:56 +0100

    z

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://plateforme-collaborative-backend-git-main-liberts-projects.vercel.app/api/:path*",
+        destination: "http://localhost:3001/api/:path*",
       },
     ];
   },
